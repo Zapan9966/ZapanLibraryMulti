f47367920209f1e82b27637eb8085d4b39a44840
Da Silva Benoit <benoit.dasilva99@gmail.com>
2023-07-18 16:31:16 +0200

    add sealed

--- ZapanControls/Controls/ListViewItem.cs
+++ ZapanControls/Controls/ListViewItem.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using ZapanControls.Controls.ControlEventArgs;
 using ZapanControls.Helpers;
 
 namespace ZapanControls.Controls
 {
-    public class ListViewItem : System.Windows.Controls.ListViewItem
+    public sealed class ListViewItem : System.Windows.Controls.ListViewItem
     {
         #region Internal Event Handlers
         private void OnMouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
